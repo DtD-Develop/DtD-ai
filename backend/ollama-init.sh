3cce968e30cb1a4563b1da71d2f612f926641365
DtD-Develop <teerit@doortodoorlogistic.com>
2025-12-02 12:46:02 +0700

    commit update pull model

--- backend/ollama-init.sh
+++ backend/ollama-init.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+echo ">>> Starting Ollama..."
+ollama serve &
+
+# wait server
+sleep 5
+
+echo ">>> Checking required models..."
 ollama list | grep "llama3.1:8b" > /dev/null || ollama pull llama3.1:8b
 ollama list | grep "nomic-embed-text" > /dev/null || ollama pull nomic-embed-text
-exec ollama serve
+
+wait -n
