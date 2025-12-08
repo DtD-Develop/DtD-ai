49ad83d8e1a23bb20da5a64a9f67a7c0d5dac3c3
DtD-Develop <teerit@doortodoorlogistic.com>
2025-12-08 11:55:38 +0700

    commit update cache path

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,6 +11,9 @@ composer install --no-dev --optimize-autoloader
 php artisan key:generate --force
 php artisan migrate --force
 
+RUN chmod -R 775 storage bootstrap/cache \
+    && chown -R www-data:www-data storage bootstrap/cache
+
 chown -R www-data:www-data storage bootstrap/cache
 
 exec php-fpm
