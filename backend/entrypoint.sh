d4739542944fe92d5b06a220d89717fa7d334120
DtD-Develop <teerit@doortodoorlogistic.com>
2025-12-08 13:02:24 +0700

    commit fix backend error

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,9 +11,6 @@ composer install --no-dev --optimize-autoloader
 php artisan key:generate --force
 php artisan migrate --force
 
-RUN chmod -R 775 storage bootstrap/cache \
-    && chown -R www-data:www-data storage bootstrap/cache
-
 chown -R www-data:www-data storage bootstrap/cache
 
 exec php-fpm
